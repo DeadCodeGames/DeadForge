65303cee9b5320d4e0d334d390037c19db9ce76f
出口レイ <richard@kanshen.click>
2025-04-09 21:05:15 +0200

    big changes hehe
    am proud :3
    o ye i did this in... about 5 hours :3

--- public/preload.js
+++
@@ -1,9 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  minimize: () => ipcRenderer.send('window-control', 'minimize'),
-  maximize: () => ipcRenderer.send('window-control', 'maximize'),
-  close: () => ipcRenderer.send('window-control', 'close'),
-  isMaximized: () => ipcRenderer.invoke('is-window-maximized'),
-});
---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect, createContext } from "react";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import WinControls from './components/WinControls/WinControls.tsx';
+
+export const AppContext = createContext<any>({ preferences: { theme: 'dark' } });
+
+function AppContextProvider({ children }: { children: React.ReactNode }) {
+  const [context, setContext] = useState<any>({ preferences: { theme: 'dark' } });
+  const [shouldSetContext, setShouldSetContext] = useState<boolean>(false);
+
+/*  useEffect(() => {
+    const fetchPreferences = async () => {
+      try {
+        const prefs = await window.Electron.getPreferences();
+        setContext({ preferences: prefs });
+        console.log(prefs)
+        setShouldSetContext(true);
+      } catch (error) {
+        console.error('Failed to fetch client preferences:', error);
+      }
+    };
+
+    fetchPreferences();
+  }, []);*/
+
+  useEffect(() => {
+    if (shouldSetContext) window.Electron.updatePreferences(context.preferences)
+  }, [context, shouldSetContext]);
+
+  return (
+    <AppContext.Provider value={{ context, setContext }}>
+      {children}
+    </AppContext.Provider>
+  ) as React.JSX.Element;
+}
+export default function App() {
+  const [platform, setPlatform] = useState<"Linux" | "Windows" | "Mac" | null>(null);
+  /*useEffect(() => {
+    async function getPlatform() {
+      const platform = await window.Electron.getPlatform();
+      setPlatform(platform);
+    }
+    getPlatform();
+  }, []);*/
+
+  return (
+    <AppContextProvider>
+      <Router>
+        <WinControls type={platform} />
+        <div id="app">
+          <Routes>
+            <Route path="/" />
+          </Routes>
+        </div>
+      </Router>
+    </AppContextProvider>
+
+  ) as React.JSX.Element;
+}
