f69190f491d09d12d9a9c1c34477d6a2a3b88534
出口レイ <richard@kanshen.click>
2025-04-19 14:26:22 +0200

    fonts cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
                 "consolas": ["Consolas", "Courier New", 'Courier', 'monospace'],
                 "uniSansCAPS": ["Uni Sans CAPS", ...defaultTheme.fontFamily.sans],
                 "montserrat": ["Montserrat", "Noto Sans JP", ...defaultTheme.fontFamily.sans],
+                "notoSans": ["Noto Sans", "Noto Sans JP", "Noto Sans KR", "Noto Sans SC", "Noto Sans TC", "Noto Sans Hebrew", ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
                 "0": "0px",
