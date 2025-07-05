abbf33bc9c9ede82a5c6b53d4de093d029995ae1
出口レイ <richard@kanshen.click>
2025-07-05 10:57:35 +0200

    added some extra error handling to the homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,13 +6,15 @@ import MarkdownText from '@/components/CustomElements/MarkdownText';
 import i18n, { dateFNSResources } from '@/locales/i18n';
 import { Trans, useTranslation } from 'react-i18next';
 import { useMediaQuery } from "react-responsive"
+import { TriangleAlert } from "lucide-react";
 import Tooltip from '@/components/CustomElements/Tooltip';
 import GameCard from '../Library/components/GameCard';
 
 const Home = () => {
     const [articles, setArticles] = useState<Article[]>([]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+    const [errorUpdating, setErrorUpdating] = useState<string | null>(null);
+    const [errorLoading, setErrorLoading] = useState<string | null>(null);
     const { games, gameJoins, favourites, launchTimestamps } = useContext(LibraryContext)
 
     const { t } = useTranslation();
@@ -23,27 +25,20 @@ const Home = () => {
         const fetchArticles = async () => {
             try {
                 // First try to update articles
-                window.Electron.updateArticles().then(async (res) => {
-                    if (res.success) {
-                        try {
-                            const updatedArticles = await window.Electron.getArticles()
-                            setArticles(updatedArticles.articles);
-                            setError(null)
-                        } catch (error) {
-                            setError('Failed to load articles');
-                        }
-                    } else if (res.error) {
-                        setError(res.error);
-                    }
-                });
+                const updatingResult = await window.Electron.updateArticles()
+                if (updatingResult.success) {
+                    setErrorUpdating(null)
+                } else if (updatingResult.error) {
+                    setErrorUpdating(updatingResult.error);
+                }
 
                 // Get articles regardless of update success
                 const result = await window.Electron.getArticles();
                 console.log(result)
                 setArticles(result.articles);
-                setError(null);
+                setErrorLoading(null);
             } catch (err) {
-                setError('Failed to load articles');
+                setErrorLoading((err as Error).message);
                 console.error('Error loading articles:', err);
             } finally {
                 setLoading(false);
@@ -96,111 +91,116 @@ const Home = () => {
 
     console.log(recentGames);
 
-    if (loading) {
-        return (
-            <div className="flex items-center justify-center min-h-screen">
-                <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-neutral-900 dark:border-white"></div>
-            </div>
-        );
-    }
-
-    if (error) {
-        return (
-            <div className="flex flex-col items-center justify-center min-h-screen text-red-600">
-                <h2 className="text-xl font-bold mb-2">Error</h2>
-                <p>{error}</p>
-            </div>
-        );
-    }
-
     return (
         <div className="w-[calc(100%-2.5rem)] flex flex-col-reverse 2xl:flex-row h-full py-6 2xl:py-0 px-6 2xl:pr-4 bg-fullMoon dark:bg-night overflow-y-auto 2xl:overflow-y-hidden">
             <div className='flex flex-col w-full 2xl:max-w-[calc(100%-257px)] gap-y-4 overflow-y-auto pt-4 2xl:pt-6 py-6 pr-6 border-0 border-solid border-notQuiteBlack/20 dark:border-notQuiteWhite/20 2xl:border-r h-[calc(100%-3rem)]'>
                 <h1 className="font-bold text-3xl pl-2 font-heading">
-                    <Trans
-                        i18nKey="home.newsAndUpdatesHeader"
-                        components={{ bold: <span className="font-uniSansCAPS font-bold" /> }}
-                    />
-                </h1>
-                <div className='space-y-6'>
-                    {articles.map((article) => (
-                        <article
-                            key={article.slug}
-                            className="bg-white dark:bg-neutral-800 rounded-2xl shadow-lg overflow-hidden"
-                        >
-                            <img
-                                src={`local://${article.bannerImage.replace("%USERDATA%", "CONST_USERDATA")}`}
-                                alt={article.title}
-                                className="w-full h-64 object-cover"
+                    <div className="flex flex-row gap-2 items-center">
+                        <span>
+                            <Trans
+                                i18nKey="home.newsAndUpdatesHeader"
+                                components={{ bold: <span className="font-uniSansCAPS font-bold" /> }}
                             />
-                            <div className="p-8">
-                                <h1 className="text-4xl font-bold mb-6 flex flex-row gap-6 items-center font-heading">
-                                    {article.title}
-                                    {/* Tags */}
-                                    <div className="flex flex-wrap gap-2 items-center justify-end -mt-0.5">
-                                        {article.tags.map((tag) => (
-                                            <span
-                                                key={tag}
-                                                className="px-3 py-1 bg-neutral-200 dark:bg-neutral-700 rounded-full text-sm inline-flex flex-row font-montserrat uppercase text-nowrap"
-                                            >
-                                                {tag}
-                                            </span>
-                                        ))}
-                                    </div>
-                                </h1>
-                                <div className="flex flex-row gap-2 mb-6">
-                                    {/* Profile pictures row */}
-                                    <div className="flex -space-x-4 hover:-space-x-1 *:transition-[margin] duration-200 ease-in-out">
-                                        {article.authors.map((author) => (
-                                            <Tooltip key={author.name} content={author.name}>
-                                                <img
-                                                    key={author.name}
-                                                    src={`local://${author.profilePicture.replace("%USERDATA%", "CONST_USERDATA")}`}
-                                                    alt={author.name}
-                                                    className="w-10 h-10 -my-1 rounded-full border-4 border-solid border-white dark:border-neutral-800"
-                                                />
-                                            </Tooltip>
-                                        ))}
-                                    </div>
-                                    <div className="flex flex-col">
-                                        {/* Author names row */}
-                                        <div className="inline">
-                                            <Trans i18nKey="home.articles.writtenBy" count={article.authors.length}>
-                                                <>
-                                                    {article.authors.map((author, index) => (
-                                                        <React.Fragment key={author.name}>
-                                                            <a
-                                                                href={author.link}
-                                                                target="_blank"
-                                                                rel="noopener noreferrer"
-                                                                className="text-blue-600 dark:text-blue-400 no-underline hover:underline font-medium m-0"
-                                                            >
-                                                                {author.name}
-                                                            </a>
-                                                            <span className="text-neutral-600 dark:text-neutral-400">{index < article.authors.length - 1 ? (index < article.authors.length - 2 ? t('commaSeparator') : t('ampersandSeparator')) : ""}</span>
-                                                        </React.Fragment>
-                                                    ))}
-                                                </>
-                                            </Trans>
+                        </span>
+                        {errorUpdating && (
+                            <Tooltip content={<span>{t("home.errorWithUpdatingArticles")}<br /><code className="px-1 py-0.5 dark:bg-night bg-fullMoon text-notQuiteBlack dark:text-notQuiteWhite rounded-md border border-solid dark:border-fullMoon/25 border-night/25">{errorUpdating}</code>{errorUpdating.startsWith("getaddrinfo") && (<><br />{t("home.errorWithUpdatingArticles-NetworkError")}</>)}</span>} position='bottom'>
+                                <TriangleAlert className='text-warning' />
+                            </Tooltip>
+                        )}
+                    </div>
+                </h1>
+                {
+                    loading ? (
+                        <div className="flex items-center justify-center min-h-[calc(100%-102px)]">
+                            <div className="rounded-full h-8 w-8 border-t-2 border-b-2 border-neutral-900 dark:border-white">{t("loading")}</div>
+                        </div>
+                    ) : errorLoading ? (
+                        <div className="flex flex-col items-center justify-center min-h-[calc(100%-102px)] text-red-600">
+                                <h2 className="text-xl font-bold mb-2">{t("errorWithLoadingArticles")}</h2>
+                            <p>{errorLoading}</p>
+                        </div>
+                    ) : (
+                        <div className='space-y-6'>
+                            {articles.map((article) => (
+                                <article
+                                    key={article.slug}
+                                    className="bg-white dark:bg-neutral-800 rounded-2xl shadow-lg overflow-hidden"
+                                >
+                                    <img
+                                        src={`local://${article.bannerImage.replace("%USERDATA%", "CONST_USERDATA")}`}
+                                        alt={article.title}
+                                        className="w-full h-64 object-cover"
+                                    />
+                                    <div className="p-8">
+                                        <h1 className="text-4xl font-bold mb-6 flex flex-row gap-6 items-center font-heading">
+                                            {article.title}
+                                            {/* Tags */}
+                                            <div className="flex flex-wrap gap-2 items-center justify-end -mt-0.5">
+                                                {article.tags.map((tag) => (
+                                                    <span
+                                                        key={tag}
+                                                        className="px-3 py-1 bg-neutral-200 dark:bg-neutral-700 rounded-full text-sm inline-flex flex-row font-montserrat uppercase text-nowrap"
+                                                    >
+                                                        {tag}
+                                                    </span>
+                                                ))}
+                                            </div>
+                                        </h1>
+                                        <div className="flex flex-row gap-2 mb-6">
+                                            {/* Profile pictures row */}
+                                            <div className="flex -space-x-4 hover:-space-x-1 *:transition-[margin] duration-200 ease-in-out">
+                                                {article.authors.map((author) => (
+                                                    <Tooltip key={author.name} content={author.name}>
+                                                        <img
+                                                            key={author.name}
+                                                            src={`local://${author.profilePicture.replace("%USERDATA%", "CONST_USERDATA")}`}
+                                                            alt={author.name}
+                                                            className="w-10 h-10 -my-1 rounded-full border-4 border-solid border-white dark:border-neutral-800"
+                                                        />
+                                                    </Tooltip>
+                                                ))}
+                                            </div>
+                                            <div className="flex flex-col">
+                                                {/* Author names row */}
+                                                <div className="inline">
+                                                    <Trans i18nKey="home.articles.writtenBy" count={article.authors.length}>
+                                                        <>
+                                                            {article.authors.map((author, index) => (
+                                                                <React.Fragment key={author.name}>
+                                                                    <a
+                                                                        href={author.link}
+                                                                        target="_blank"
+                                                                        rel="noopener noreferrer"
+                                                                        className="text-blue-600 dark:text-blue-400 no-underline hover:underline font-medium m-0"
+                                                                    >
+                                                                        {author.name}
+                                                                    </a>
+                                                                    <span className="text-neutral-600 dark:text-neutral-400">{index < article.authors.length - 1 ? (index < article.authors.length - 2 ? t('commaSeparator') : t('ampersandSeparator')) : ""}</span>
+                                                                </React.Fragment>
+                                                            ))}
+                                                        </>
+                                                    </Trans>
+                                                </div>
+                                                {/* Dates */}
+                                                <div className="text-xs text-neutral-600 dark:text-neutral-400">
+                                                    {t('home.articles.publishedOn', { date: format(new Date(article.publishDate), 'PPPp', { locale: dateFNSResources[i18n.language as keyof typeof dateFNSResources] }) })}
+                                                    {article.lastModified && article.publishDate !== article.lastModified && (
+                                                        <>{t('commaSeparator')}{t('home.articles.lastEditedOn', { date: format(new Date(article.lastModified), 'PPPp', { locale: dateFNSResources[i18n.language as keyof typeof dateFNSResources] }) })}</>
+                                                    )}
+                                                </div>
+                                            </div>
                                         </div>
-                                        {/* Dates */}
-                                        <div className="text-xs text-neutral-600 dark:text-neutral-400">
-                                            {t('home.articles.publishedOn', { date: format(new Date(article.publishDate), 'PPPp', { locale: dateFNSResources[i18n.language as keyof typeof dateFNSResources] }) })}
-                                            {article.lastModified && article.publishDate !== article.lastModified && (
-                                                <>{t('commaSeparator')}{t('home.articles.lastEditedOn', { date: format(new Date(article.lastModified), 'PPPp', { locale: dateFNSResources[i18n.language as keyof typeof dateFNSResources] }) })}</>
-                                            )}
+                                        <div className="">
+                                            <MarkdownText mediaMap={article.assetsMap} className='font-notoSans'>
+                                                {article.content}
+                                            </MarkdownText>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="">
-                                    <MarkdownText mediaMap={article.assetsMap} className='font-notoSans'>
-                                        {article.content}
-                                    </MarkdownText>
-                                </div>
-                            </div>
-                        </article>
-                    ))}
-                </div>
+                                </article>
+                            ))}
+                        </div>
+                    )
+                }
             </div>
             <div className='flex flex-col gap-2 2xl:w-[216px] overflow-y-visible overflow-x-visible 2xl:p-4 !pr-0 border-solid border-0 border-b 2xl:border-b-0 border-notQuiteBlack/20 dark:border-notQuiteWhite/20'>
                 <h1 className="block font-bold text-2xl hHLPLFVBBT2XLS:-mt-2 hHLPLFVBBT2XLS:mb-1 font-heading w-[216px]">{t("home.jumpBackIn")}</h1>
