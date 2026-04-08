a898ed7edf71562e56a99229717d2b269be040e7
DeadCode Service Bot ×× <ghservicesbot@mail.deadcode.is-a.dev>
2026-04-08 13:06:47 +0200

    chore(i18n): sync Crowdin translations

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -4,8 +4,32 @@ import { enUS, cs, sk, ja, fr, it, zhCN, zhTW, de, ko, Locale } from 'date-fns/l
 import {unflatten, flatten} from 'flat';
 
 import en_001 from './en_001.json';
+import cs_CZ from './cs_CZ.json';
+import de_DE from './de_DE.json';
+import en_PT from './en_PT.json';
+import fr_FR from './fr_FR.json';
+import it_IT from './it_IT.json';
+import ja_JP from './ja_JP.json';
+import ko_KR from './ko_KR.json';
+import lol_US from './lol_US.json';
+import sk_SK from './sk_SK.json';
+import uk_UA from './uk_UA.json';
+import zh_CN from './zh_CN.json';
+import zh_TW from './zh_TW.json';
 const resources: Record<string, { translation: object }> = {
+    cs_CZ: { translation: cs_CZ },
+    de_DE: { translation: de_DE },
     en_001: { translation: en_001 },
+    en_PT: { translation: en_PT },
+    fr_FR: { translation: fr_FR },
+    it_IT: { translation: it_IT },
+    ja_JP: { translation: ja_JP },
+    ko_KR: { translation: ko_KR },
+    lol_US: { translation: lol_US },
+    sk_SK: { translation: sk_SK },
+    uk_UA: { translation: uk_UA },
+    zh_CN: { translation: zh_CN },
+    zh_TW: { translation: zh_TW },
 };
 const dateFNSResources: Record<keyof typeof resources, Locale> = {
     en_001: enUS,
